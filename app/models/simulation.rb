c40e7e4db619f6b9e07d2c19c31b71ecabca3fb8
Luan Andrade <luansandrade92@hotmail.com>
2016-07-23 15:47:14 -0300

    validações e métodos no model

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,3 +1,11 @@
 class Simulation < ActiveRecord::Base
   belongs_to :user
+
+  def user_name
+    if self.user.blank?
+      "-"
+    else
+      self.user.name
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
 	has_many :simulations
+
+	validates :name, presence: true
+	validates :email, uniqueness: true, presence: true
+
+	def teste
+		"teste teste"
+	end
 end
